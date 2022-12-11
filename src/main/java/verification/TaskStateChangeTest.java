2b76ec58fc846093d74e1ea62a965b5cfc7a39d2
Ekaterina Moreva <ekaterina.moreva@vk.team>
2022-12-11 21:12:22 +0300

    wip

--- src/main/java/verification/TaskStateChangeTest.java
+++ src/main/java/verification/TaskStateChangeTest.java
@@ -3,7 +3,7 @@ package verification;
 import OS.OrtOS;
 import Tasks.Task;
 
-public class ReadyRunningTest {
+public class TaskStateChangeTest {
     public static void main(String[] args) {
         final OrtOS os = new OrtOS();
         final Task taskToStart = new Task(0, Thread.MAX_PRIORITY, os);
