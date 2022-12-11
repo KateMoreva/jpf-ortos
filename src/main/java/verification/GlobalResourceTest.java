13b98fd112fb853ab6e9fbdc6ec9ae6c0b890fdd
Ekaterina Moreva <ekaterina.moreva@vk.team>
2022-12-11 17:52:41 +0300

    wip

--- src/main/java/verification/GlobalResourceTest.java
+++ src/main/java/verification/GlobalResourceTest.java
@@ -11,6 +11,9 @@ public class GlobalResourceTest {
         final OrtOS os = new OrtOS();
         final Task taskToStart = new Task(0, Thread.MAX_PRIORITY, os);
         os.startOS(taskToStart);
+        for (int i = 0; i < 17; i++) {
+            os.declareResource(100 + i, true);
+        }
         os.printSystemInfo();
         os.shutdownOS();
         assert os.info.getGlobalResourcesDeclared() <= GLOBAL_RESOURCES_COUNT;
